e4aabc83ed8a4a7144eeb4b537a544cfc8f988d1
saket <saket0698@gmail.com>
2020-09-03 14:12:09 +0530

    updating files

--- sentimentanalysis/sentiment analysis.py
+++ sentimentanalysis/sentiment analysis.py
@@ -1,5 +1,10 @@
 import tweepy
 from textblob import TextBlob
+
+
+
+
+
 consumer_key = ""
 consumer_key_secret = ""
 
@@ -8,21 +13,50 @@ access_token_secret = ""
 
 auth = tweepy.OAuthHandler(consumer_key,consumer_key_secret)
 
-
 auth.set_access_token(access_token,access_token_secret)
 
 api = tweepy.API(auth)
 
-x = str(input("Enter Keyword"))
-public_tweets = api.search(x)
+def percentage(part,whole):
+    return 100 * float(part)/float(whole)
 
-for tweet in public_tweets:
+x = str(input("Enter Keyword"))
+n = int(input("Enter number of tweets to be analyzed"))
+
+
+tweets = tweepy.cursor(api.search,q=x).items(n)
+
+positive,negative,neutral,polarity = 0
+
+
+for tweet in tweets:
+    
     print(tweet.text)
+    
     analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
-    if(analysis.sentiment[0]>0):
-        print("positive")
-    elif(analysis.sentiment[0]<0):
-        print("negative")
+    pol = analysis.sentiment.polarity
+    
+    polarity + = pol
+
+    
+    if(pol>0):
+        positive += 1
+    elif(pol<0):
+        negative += 1
     else:
-        print("neutral")
+        neutral += 1
+
+
+positive = percentage(positive,n)
+negative = percentage(negative,n)
+neutral  = percentage(neutral,n)
+
+
+print("people's reaction on" + x +" by analyzing" + n + " Tweets.")
+if(polarity>0):
+    print("Positive")
+elif(polarity<0):
+    print("Negative")
+else:
+    print("Neutral")
+
